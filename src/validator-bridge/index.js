eb001b17cb895f45fcc336e2fdd628a82fd9f08e
mathijsvanvelde <mathijs@opmaatzagen.nl>
2021-08-24 15:52:33 +0200

    Ran `npm audit fix`

--- src/validator-bridge/index.js
+++ src/validator-bridge/index.js
@@ -23,34 +23,36 @@
 // - `uuid`
 
 // const validator = require("validator");
-import dashify from "dashify";
+import dashify from 'dashify';
+
 //const camelCase = require("camelcase");
-import camelCase from "uppercamelcase";
-import { addMethod, array, boolean, date, mixed, number, string } from "yup";
+import camelCase from 'uppercamelcase';
+
+import { addMethod, array, boolean, date, mixed, number, string } from 'yup';
 
 const toConstraintsMap = (values, opts = {}) => {
-	return values.reduce((acc, value) => {
-		if (typeof value !== "string" && !(value instanceof Object)) {
-			if (opts.throws !== false) {
-				throw `toConstraintsMap: invalid entry ${value}`;
-			} else {
-				return acc;
-			}
-		}
-		if (typeof value === "string") {
-			acc[name] = {};
-		} else {
-			if (!value.name) {
-				if (opts.throws !== false) {
-					throw `toConstraintsMap: invalid entry ${value} missing name`;
-				} else {
-					return acc;
-				}
-			}
-			acc[value.name] = value;
-		}
-		return acc;
-	}, {});
+  return values.reduce((acc, value) => {
+    if (typeof value !== "string" && !(value instanceof Object)) {
+      if (opts.throws !== false) {
+        throw `toConstraintsMap: invalid entry ${value}`;
+      } else {
+        return acc;
+      }
+    }
+    if (typeof value === "string") {
+      acc[name] = {};
+    } else {
+      if (!value.name) {
+        if (opts.throws !== false) {
+          throw `toConstraintsMap: invalid entry ${value} missing name`;
+        } else {
+          return acc;
+        }
+      }
+      acc[value.name] = value;
+    }
+    return acc;
+  }, {});
 };
 
 // const defaultConstraints = [
@@ -63,37 +65,37 @@ const toConstraintsMap = (values, opts = {}) => {
 // ]
 
 const defaultConstraints = {
-	alphanumeric: {
-		optsKey: "locale",
-	},
-	alpha: {
-		optsKey: "locale",
-	},
-	ascii: {},
-	byte: {},
-	creditCard: {},
-	currency: {
-		opts: "currencyOpts",
-	},
-	dataUri: {},
-	dateTime: {},
-	date: {},
-	domainName: {
-		opts: "domainOpts",
-	},
-	hash: {
-		opts: "hashAlgo",
-	},
-	hexColor: {},
-	ipv4: {},
-	ipv6: {},
-	isbn: {},
-	magnetUri: {},
-	mimeType: {},
-	mobilePhone: {},
-	mongoId: {},
-	postalCode: {},
-	uuid: {},
+  alphanumeric: {
+    optsKey: "locale"
+  },
+  alpha: {
+    optsKey: "locale"
+  },
+  ascii: {},
+  byte: {},
+  creditCard: {},
+  currency: {
+    opts: "currencyOpts"
+  },
+  dataUri: {},
+  dateTime: {},
+  date: {},
+  domainName: {
+    opts: "domainOpts"
+  },
+  hash: {
+    opts: "hashAlgo"
+  },
+  hexColor: {},
+  ipv4: {},
+  ipv6: {},
+  isbn: {},
+  magnetUri: {},
+  mimeType: {},
+  mobilePhone: {},
+  mongoId: {},
+  postalCode: {},
+  uuid: {}
 };
 
 // Template:
@@ -109,108 +111,109 @@ const defaultConstraints = {
 // });
 
 const defaults = {
-	createValidatorName: (validatorName, key) => {
-		const name = validatorName || key;
-		validatorName = camelCase(name);
-		validatorName = validatorName.replace(/Uri$/, "URI");
-		validatorName = validatorName.replace(/Id$/, "ID");
-		return `is${validatorName}`;
-	},
-	createTestName: (testName, key) => (testName = dashify(testName || key)),
+  createValidatorName: (validatorName, key) => {
+    const name = validatorName || key;
+    validatorName = camelCase(name);
+    validatorName = validatorName.replace(/Uri$/, "URI");
+    validatorName = validatorName.replace(/Id$/, "ID");
+    return `is${validatorName}`;
+  },
+  createTestName: (testName, key) => (testName = dashify(testName || key))
 };
 
 const fallBackFnMap = {
-	isMagnetURI: (value, isMagnetUri) => {
-		return /magnet:\?xt=urn:[a-z0-9]+:[a-z0-9]{32}/i.test(value);
-	},
+  isMagnetURI: (value, isMagnetUri) => {
+    return /magnet:\?xt=urn:[a-z0-9]+:[a-z0-9]{32}/i.test(value);
+  }
 };
 
 function extendYupApi({
-	constraints,
-	override = false,
-	validator,
-	createValidatorName,
-	createTestName,
-	schemaType = string,
+  constraints,
+  override = false,
+  validator,
+  createValidatorName,
+  createTestName,
+  schemaType
 } = {}) {
-	if (!validator) {
-		throw "extendYupApi: missing validator option";
-	}
+  if (!validator) {
+    throw "extendYupApi: missing validator option";
+  }
 
-	if (Array.isArray(constraints)) {
-		constraints = toConstraintsMap(constraints);
-	}
+  if (Array.isArray(constraints)) {
+    constraints = toConstraintsMap(constraints);
+  }
 
-	if (!override) {
-		constraints = {
-			...defaultConstraints,
-			...(constraints || {}),
-		};
-	} else {
-		constraints = constraints || defaultConstraints;
-	}
+  if (!override) {
+    constraints = {
+      ...defaultConstraints,
+      ...(constraints || {})
+    };
+  } else {
+    constraints = constraints || defaultConstraints;
+  }
 
-	schemaType =
-		typeof schemaType === "string"
-			? ((schemaType) => {
-					switch (schemaType) {
-						case "mixed":
-							return mixed;
-						case "string":
-							return string;
-						case "number":
-							return number;
-						case "boolean":
-						case "bool":
-							return boolean;
-						case "date":
-							return date;
-						case "array":
-							return array;
-					}
-			  })(schemaType)
-			: schemaType;
+  schemaType =
+    typeof schemaType === "string"
+      ? (schemaType => {
+          switch (schemaType) {
+            case "mixed":
+              return mixed;
+            case "string":
+              return string;
+            case "number":
+              return number;
+            case "boolean":
+            case "bool":
+              return boolean;
+            case "date":
+              return date;
+            case "array":
+              return array;
+          }
+        })(schemaType)
+      : (schemaType ?? string);
 
-	createValidatorName = createValidatorName || defaults.createValidatorName;
-	createTestName = createTestName || defaults.createTestName;
+  createValidatorName = createValidatorName || defaults.createValidatorName;
+  createTestName = createTestName || defaults.createTestName;
 
-	Object.keys(constraints).map((key) => {
-		let { testName, optsKey, validatorName, logging } = constraints[key];
-		const fullValidatorName = createValidatorName(validatorName, key);
-		testName = createTestName(testName, key);
+  Object.keys(constraints).map(key => {
+    let { testName, optsKey, validatorName, logging } = constraints[key];
+    const fullValidatorName = createValidatorName(validatorName, key);
+    testName = createTestName(testName, key);
 
-		// See https://github.com/jquense/yup#yupaddmethodschematype-schema-name-string-method--schema-void
-		addMethod(schemaType, key, (args = {}) => {
-			const { message } = args;
-			const opts = args[optsKey];
-			return schemaType().test(testName, message, (value) => {
-				// return this.transform(value => {
-				const { path, createError } = this;
-				// [value] - value of the property being tested
-				// [path]  - property name,
-				// ...
-				let validatorFn = validator[fullValidatorName];
-				validatorFn = validatorFn || fallBackFnMap[fullValidatorName];
+    // See https://github.com/jquense/yup#yupaddmethodschematype-schema-name-string-method--schema-void
+    addMethod(schemaType, key, (args = {}) => {
+      const { message } = args;
+      const opts = args[optsKey];
+      return schemaType().test(testName, message, value => {
+        // return this.transform(value => {
+        const { path, createError } = this;
+        // [value] - value of the property being tested
+        // [path]  - property name,
+        // ...
+        let validatorFn = validator[fullValidatorName];
+        validatorFn = validatorFn || fallBackFnMap[fullValidatorName];
 
-				if (typeof validatorFn !== "function") {
-					throw Error(
-						"No method named ${validatorName} on validator"
-					);
-				}
-				const valid = validatorFn(value, opts);
-				if (logging === true) {
-					console.log("Yup validator bridge", {
-						key,
-						fullValidatorName,
-						testName,
-						value,
-						valid,
-					});
-				}
-				return valid || createError({ path, message });
-			});
-		});
-	});
+        if (typeof validatorFn !== "function") {
+          throw Error("No method named ${validatorName} on validator");
+        }
+        const valid = validatorFn(value, opts);
+        if (logging === true) {
+          console.log("Yup validator bridge", {
+            key,
+            fullValidatorName,
+            testName,
+            value,
+            valid
+          });
+        }
+        return valid || createError({ path, message });
+      });
+    });
+  });
 }
 
-export { extendYupApi, toConstraintsMap };
+export {
+  extendYupApi,
+  toConstraintsMap
+};
